86578e63a98a3cbb5537344f76e820f4f068e163
thisAKcode <alekseikupiakov@hotmail.com>
2022-10-21 23:05:27 +0200

    dotenv

--- app/config.py
+++ app/config.py
@@ -1,22 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from pathlib import Path
+from dotenv import load_dotenv
 import urllib.parse
 
+load_dotenv()
 
-pw = urllib.parse.quote_plus("ljlkd%fFDFw12?Dg0vRiF")
+pw = urllib.parse.quote_plus(os.environ.get('pw_pg'))
 
 #  conn = psycopg2.connect(     host="localhost",     database="python_db",     user="postgres",     password="ljlkd%fFDFw12?Dg0vRiF")
 # postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
-# DATABASE_URL = fr"postgresql://postgres:{pw}@localhost:5432/python_db"
 #DATABASE_URL = f'postgresql://postgres:{pw}@localhost/python_db'
 DATABASE_URL = 'sqlite:///mydb.db'
-# DATABASE_URL2 = fr"postgresql:\\postgres:{pw}@localhost\python_db"
 
 engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-print(DATABASE_URL, Path(DATABASE_URL))
-print(type(DATABASE_URL), type(Path(DATABASE_URL)))
 
