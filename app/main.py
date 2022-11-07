111c7943c3fb1e7eeb4208c83b2ad63e091d8b97
thisAKcode <alekseikupiakov@hotmail.com>
2022-11-07 09:27:17 +0100

    attempt to pass json to js in map2.html

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import model
 import router
+import json
 from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from config import SessionLocal, engine, get_db
 from populate_db import insert_items, data_loader, DUMMY_DATA
@@ -14,6 +16,22 @@ model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://localhost:8000/map/"
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # populate_db if records are not already there.
 @app.on_event("startup")
@@ -35,5 +53,14 @@ async def _map(
 @app.post("/map/")
 async def pass_item():
     return 'abc'
+
+@app.get("/map2/", response_class=HTMLResponse)
+async def _map(
+    request:Request,
+    db:Session = Depends(get_db)
+    ):
+    #locations1 = db.query(model.ArtItem).all() # []
+    json_object = json.dumps({"message": "fine"}, indent = 4)
+    return json_object
 
 app.include_router(router.app, tags=["art"])
